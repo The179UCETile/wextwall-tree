8d7f44b10c841ec45a90d1261e25b23ae7aabd15
The179UCETile <the179ucetile@proton.me>
2026-08-27 08:20:27 +0700

    e

--- js/mod.js
+++ js/mod.js
@@ -17,9 +17,8 @@ let VERSION = {
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.0 - Release</h3><br>
+		- Added development layer and TextWall layer.<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
